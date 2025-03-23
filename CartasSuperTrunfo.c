6fcb46e3b210250112e6d66750c731d28717b06d
LostYety <olvgbb31@gmail.com>
2025-03-23 17:50:44 +0000

    Versão final do Código(nível iniciante)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,36 +9,89 @@
 
 int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    char cod_carta[4];
-    char nome_cidade[50];
-    double populacao;
-    int area;
-    double pib;
+    char cod_cidade[4], cod_cidade2[4];
+    char nome_cidade[50], nome_cidade2[50];
+    int populacao, populacao2;
+    float area, area2;
+    float pib, pib2;
+    int ponto_turistico, ponto_turistico2;
 
+    printf("JOGO TRUNFO TEMA: PAÍSES\n");
 
+    // Cadastro da Cidade 1
     printf("Insira o cód da cidade:\n");
-    scanf("%3s", cod_carta);
-    printf("Código inserido: %s\n", cod_carta);
-
-     // Limpa o buffer de entrada
-    while (getchar() != '\n');
+    scanf("%3s", cod_cidade);
+    printf("Código inserido: %s\n", cod_cidade);
+    while (getchar() != '\n'); // Limpa o buffer
 
     printf("Insira o nome da cidade:\n");
-    scanf("%49[^\n]s", nome_cidade);
+    scanf("%49[^\n]", nome_cidade); // Corrigido: removido o 's' extra
     printf("Nome de Cidade inserido: %s\n", nome_cidade);
-    
-    printf("Insira o tamanho da população :\n "); //CONCERTAR PARTE DOS NÚMEROS
-    scanf("%lf", &populacao);
-    printf("Numero da população inserido: %.3lf\n", populacao);
+    while (getchar() != '\n');
 
-    printf("Insira o tamanho da area :\n "); //CONCERTAR PARTE DOS NÚMEROS
-    scanf("%d", &area);
-    printf("tamanho da area inserido: %d\n", area);
+    printf("Insira o tamanho da população:\n"); 
+    scanf("%d", &populacao);
+    printf("Número da população inserido: %d\n", populacao);
+
+    printf("Insira o tamanho da área:\n"); 
+    scanf("%f", &area);
+    printf("Tamanho da área inserido: %.2f\n", area);
+
+    printf("Insira o PIB:\n"); 
+    scanf("%f", &pib);         
+    printf("Número do PIB inserido: %.2f\n", pib);
+
+    printf("Insira o número de ponto turístico:\n");
+    scanf("%d", &ponto_turistico);
+    printf("Número de pontos turísticos: %d\n", ponto_turistico);
+    while (getchar() != '\n');
+
+    // Cadastro da Cidade 2
+    printf("Insira o cód da cidade2:\n");
+    scanf("%3s", cod_cidade2);
+    printf("Código inserido: %s\n", cod_cidade2);
+    while (getchar() != '\n'); // Limpa o buffer após o código
+
+    printf("Insira o nome da cidade 2:\n");
+    scanf("%49[^\n]", nome_cidade2); // Corrigido: removido o 's' extra
+    printf("Nome de Cidade inserido: %s\n", nome_cidade2);
+    while (getchar() != '\n');
+
+    printf("Insira o tamanho da população 2:\n"); 
+    scanf("%d", &populacao2);
+    printf("Número da população inserido: %d\n", populacao2);
+
+    printf("Insira o tamanho da área 2:\n"); 
+    scanf("%f", &area2);
+    printf("Tamanho da área inserido: %.2f\n", area2);
+
+    printf("Insira o PIB 2:\n"); 
+    scanf("%f", &pib2);         
+    printf("Número do PIB inserido: %.2f\n", pib2);
+
+    printf("Insira o número de ponto turístico 2:\n");
+    scanf("%d", &ponto_turistico2);
+    printf("Número de pontos turísticos: %d\n", ponto_turistico2);
+
+    // Interface de exibição
+  
+    printf("\nCIDADE 1:\n");
+    printf("Código da cidade 1: %s\n", cod_cidade);
+    printf("Nome da cidade 1: %s\n", nome_cidade);
+    printf("Tamanho da população 1: %d\n", populacao);
+    printf("Tamanho da área 1: %.2f\n", area);
+    printf("Quantidade de Pontos turísticos 1: %d\n", ponto_turistico);
+
+    printf("\nCIDADE 2:\n");
+    printf("Código da cidade 2: %s\n", cod_cidade2);
+    printf("Nome da cidade 2: %s\n", nome_cidade2);
+    printf("Tamanho da população 2: %d\n", populacao2);
+    printf("Tamanho da área 2: %.2f\n", area2);
+    printf("Quantidade de Pontos turísticos 2: %d\n", ponto_turistico2);
 
 
-    printf("Insira o PIB:\n "); //CONCERTAR PARTE DOS NÚMEROS
-    scanf("%lf", &pib);         
-    printf("Numero do PIB inserido: %lf\n", pib);
+     
+  
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
